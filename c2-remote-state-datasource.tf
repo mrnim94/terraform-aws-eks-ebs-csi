8271fb31d60611565558e6817a4764541be9dbed
mr nim <mrnim@nimtechnology.com>
2022-11-21 00:07:12 +0700

    refactor v1.0.3

--- c2-remote-state-datasource.tf
+++ c2-remote-state-datasource.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "eks" {
   backend = "s3"
   config = {
-    bucket = var.bucket_remote-state
+    bucket = var.bucket_remote_state
     key    = var.key_remote_state
     region = var.aws_region
   }
